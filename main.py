764c81badddf0f74b9c84c3de961897b63a28855
Lenacwk <lrsdance4life@gmail.com>
2020-05-18 16:54:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+num = int(input("Enter a number: "))
+print(num)
+
+if num > 10:
+  print("The number is greater than 10!")
+elif num < 10:
+  print("The number is less than 10")
+else: 
+  print("number is equal to 10")
